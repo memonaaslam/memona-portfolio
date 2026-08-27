1126e6cd6320384afab64b33d7cb2a42c282c5bd
Memona Aslam <memonaaslam00@gmail.com>
2026-08-28 03:06:52 +0500

    Add portfolio route

---
+++ src/routes/portfolio.tsx
@@ -0,0 +1,8 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { LandingPage } from "@/components/landing/page";
+
+export const Route = createFileRoute("/portfolio")({ component: Portfolio });
+
+function Portfolio() {
+  return <LandingPage />;
+}
